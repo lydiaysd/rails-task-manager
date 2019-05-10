25cb20826eac86f5ed1d50319798553fd913f6e1
Lydia Yuzva <lydiaysd@gmail.com>
2019-05-10 16:55:23 +0100

    Rails Task Manager MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  post 'tasks', to: 'tasks#create'
+  delete 'task/:id', to: 'tasks#destroy', as: :delete
 end
